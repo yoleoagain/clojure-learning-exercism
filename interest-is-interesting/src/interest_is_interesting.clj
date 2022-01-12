a75a0107c2f26aa0b75c5204558cd2b8064f1e17
yoleo@yandex.ru <yoleo@yandex.ru>
2022-01-12 12:50:31 +0200

    Refactor float numbers task

--- interest-is-interesting/src/interest_is_interesting.clj
+++ interest-is-interesting/src/interest_is_interesting.clj
@@ -3,34 +3,20 @@
 (defn interest-rate
   "TODO: add docstring"
   [balance]
-  (let [double-balance (bigdec balance)]
-    (double
-      (cond
-        (< double-balance 0) -3.213
-        (and (>= double-balance 0) (< double-balance 1000)) 0.5
-        (and (>= double-balance 1000) (< double-balance 5000)) 1.621
-        (>= double-balance 5000) 2.475
-        :else 0))))
+  (cond
+    (< balance 0.0M) -3.213
+    (< balance 1000.0M) 0.5
+    (< balance 5000.0M) 1.621
+    :else 2.475))
 
-(defn percentage-from-sum [sum percent] (bigdec (/ (* sum percent) 100)))
-
-(defn truncate-zeros
-  [balance]
-  (bigdec
-    (clojure.string/join
-      (reverse
-        (drop-while #(= % \0 ) (reverse (str balance)))) )))
+(defn percentage-from-sum [sum percent] (bigdec (/ (* sum (bigdec percent)) 100)))
 
 (defn annual-balance-update
   "TODO: add docstring"
   [balance]
   (let [rate (interest-rate balance)
         rate-in-money (percentage-from-sum balance rate)]
-    (bigdec
-      (truncate-zeros
-        (clojure.string/replace
-          (format "%.20f"
-            (+ balance (* rate-in-money (if (neg? rate) -1 1)))) \, \.)))))
+   (+ balance (* rate-in-money (if (neg? rate) -1M 1M)))))
 
 (defn amount-to-donate
   "TODO: add docstring"
